ab5ac9b36091d10817f60821df3f863dbbf41fad
Avraam Mavridis <avraammauridis@gmail.com>
2016-01-11 22:59:41 +0100

    Internal refactor of multinheritance

--- src/@multiinheritance.js
+++ src/@multiinheritance.js
@@ -6,6 +6,37 @@
 */
 import { _isFunction } from './validationHelpers';
 
+
+const __inherit = function ( _clas, _meths, _partially )
+{
+  const classes = [].concat( _clas ).reverse();
+  const methods = [].concat( _meths );
+  return function ( target )
+  {
+    classes.forEach( function ( _class ) {
+      const keys = Object.getOwnPropertyNames( _class.prototype );
+      keys.forEach( function ( key ) {
+        if ( _partially )
+        {
+          if ( !target.prototype[ key ] && methods.indexOf( key ) > -1 && _isFunction( _class.prototype[ key ] ) )
+          {
+            target.prototype[ key ] = _class.prototype[ key ];
+          }
+        }
+        else if ( !_partially )
+        {
+          if ( !target.prototype[ key ] && _isFunction( _class.prototype[ key ] ) )
+          {
+            target.prototype[ key ] = _class.prototype[ key ];
+          }
+        }
+
+      } );
+    } );
+    return target;
+  };
+};
+
 /**
  * Inherit all the methods of the passed classes
  * if two classes have method with the same name
@@ -18,22 +49,7 @@ import { _isFunction } from './validationHelpers';
  * @return { class }
  */
 export const _multiInherit = function ( ...args ) {
-  const classes = args.reverse();
-  return function ( target )
-  {
-    classes.forEach( function ( _class ) {
-      const keys = Object.getOwnPropertyNames( _class.prototype );
-      keys.forEach( function ( key ) {
-        if ( !target.prototype[ key ] )
-        {
-          if ( _isFunction( _class.prototype[ key ] ) ) {
-            target.prototype[ key ] = _class.prototype[ key ];
-          }
-        }
-      } );
-    } );
-    return target;
-  };
+  return __inherit( args, [], false );
 };
 
 /**
@@ -47,21 +63,7 @@ export const _multiInherit = function ( ...args ) {
  * @return { class }
  */
 export const _partialyInherit = function ( _clas, _meths ) {
-  const classes = [].concat( _clas ).reverse();
+  const classes = [].concat( _clas );
   const methods = [].concat( _meths );
-  return function ( target )
-  {
-    classes.forEach( function ( _class ) {
-      const keys = Object.getOwnPropertyNames( _class.prototype );
-      keys.forEach( function ( key ) {
-        if ( !target.prototype[ key ] && methods.indexOf( key ) > -1 )
-        {
-          if ( _isFunction( _class.prototype[ key ] ) ) {
-            target.prototype[ key ] = _class.prototype[ key ];
-          }
-        }
-      } );
-    } );
-    return target;
-  };
+  return __inherit( classes, methods, true );
 };
