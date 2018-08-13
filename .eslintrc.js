e447f2799070304db84fb9d1b8cfb72eb09befc9
AvraamMavridis <avraammauridis@gmail.com>
2018-08-13 08:53:14 +0200

    Chore: Update eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "avraam",
+    "rules": {
+        "class-methods-use-this": 0,
+        "import/no-unresolved": 0,
+        "import/prefer-default-export": 0,
+        "no-underscore-dangle": 0,
+        "no-param-reassign": 0
+    }
+}
